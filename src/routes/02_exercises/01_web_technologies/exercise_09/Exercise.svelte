c986e3dcef171fa0f134b28917034e847bbcfe53
MattiaBazzani <mattia.bazzani.one@gmail.com>
2023-05-08 19:49:02 +0200

    Update

---
+++ src/routes/02_exercises/01_web_technologies/exercise_09/Exercise.svelte
@@ -0,0 +1,8 @@
+<script>
+    const radius=1;
+    function circ(radius) {
+        return (radius * 2 * Math.PI).toFixed(2)
+    }
+</script>
+
+The circumference of a circle with radius {radius}m is {circ(radius)}m
