2969448eef8a0a81e29a91e57a32b85b734ab7f9
MattiaBazzani <mattia.bazzani.one@gmail.com>
2023-05-08 19:15:45 +0200

    Update

--- src/routes/02_exercises/01_web_technologies/exercise_06/Exercise.svelte
+++ src/routes/02_exercises/01_web_technologies/exercise_06/Exercise.svelte
@@ -1,1 +1,9 @@
-I really like data visualisation!
+<style>
+    p {
+        font-style: italic;
+        font-size: larger;
+        color: steelblue;
+    }
+</style>
+
+<p>I <span style="color: red;">really</span> like data visualisation!</p>
