b390deba57580e9dd39798e2d5d3a698d2bdbaeb
MattiaBazzani <mattia.bazzani.one@gmail.com>
2023-05-09 21:14:34 +0200

    Update

--- src/routes/02_exercises/01_web_technologies/exercise_14/Exercise.svelte
+++ src/routes/02_exercises/01_web_technologies/exercise_14/Exercise.svelte
@@ -3,14 +3,26 @@
 </script>
 
 <!-- We will cover buttons later on! -->
-<button class="btn btn-secondary" on:click={() => (current = "messi")}>
+<button class="btn btn-secondary" 
+  class:selected={current === "messi"}
+  on:click={() => (current = "messi")}>
   Messi
 </button>
 
-<button class="btn btn-secondary" on:click={() => (current = "ronaldo")}>
+<button class="btn btn-secondary"
+class:selected={current === "ronaldo"}
+  on:click={() => (current = "ronaldo")}>
   Ronaldo
 </button>
 
-<button class="btn btn-secondary" on:click={() => (current = "neymar")}>
+<button class="btn btn-secondary" 
+class:selected={current === "neymar"}
+  on:click={() => (current = "neymar")}>
   Neymar
 </button>
+
+<style>
+  .selected {
+    background-color: blue;
+  }
+</style>
