a9a7bff6a347cd6e5ab3920c7773369dac401e52
MattiaBazzani <mattia.bazzani.one@gmail.com>
2023-05-22 23:38:31 +0200

    update

--- src/routes/02_exercises/02_basic_visualisations/exercise_20/Exercise.svelte
+++ src/routes/02_exercises/02_basic_visualisations/exercise_20/Exercise.svelte
@@ -1,4 +1,8 @@
 <script>
+    import { scaleLinear,scaleBand } from "d3-scale";
+    import { extent } from "d3-array";
+    import { axisBottom, axisLeft } from "d3-axis";
+    import { select } from "d3-selection";
     // Dimensions
     const width = 600;
     const height = 300;
@@ -15,12 +19,28 @@
       { service: "Apple TV", viewers: 1.1 },
       { service: "Rakuten", viewers: 0.4 }
     ];
+
+    const yScale = scaleLinear() .domain([0, 3]) .range([innerHeight, 0]);
+    const xScale = scaleBand()
+    .domain(data.map((d) => d.service))
+    .range([0, innerWidth])
+    .padding(0.2);
+    const yAxis = (node) => axisLeft(yScale)(select(node));
+    const xAxis = (node) => axisBottom(xScale)(select(node));
   </script>
   
   <!-- setting a viewBox and max-width allows the SVG to shrink but not grow! -->
   <svg viewbox="0 0 {width} {height}" style="max-width: {width}px">
     <g transform={`translate(${margin.left},${margin.top})`}>
-      <!--  -->
+      {#each data as d} 
+        <rect x={xScale(d.service)} y={yScale(d.viewers)} width="40" height={innerHeight-yScale(d.viewers)}/>
+      {/each}
+      <g use:yAxis>
+        <text class="ylabel" x={-innerHeight / 2} y={-margin.left / 2}>
+          Viewers (Million)
+        </text>
+      </g>
+      <g use:xAxis transform="translate(0,{innerHeight})" />
     </g>
   </svg>
   
