e0097ba7db64fe999809f6597ccea04fdc3d7041
MattiaBazzani <mattia.bazzani.one@gmail.com>
2023-05-25 17:12:26 +0200

    update

--- src/routes/02_exercises/02_basic_visualisations/exercise_21/Exercise.svelte
+++ src/routes/02_exercises/02_basic_visualisations/exercise_21/Exercise.svelte
@@ -1,3 +1,18 @@
 <script>
-
+    import { json } from "d3-fetch";
+    import {onMount} from "svelte";
+    import Scatterplot from "./Scatterplot.svelte";
+    let data = null;
+    onMount(async () => {
+        console.log("onMount started!");
+        data = await json('/static/data/gapminder.json');
+        console.log("Data loaded!");
+    });
+    console.log("Script finished!")
 </script>
+
+{#if !data}
+    <p> loading the data, please wait...</p>
+{:else}
+    <Scatterplot data={data[0]["countries"]} />
+{/if}
