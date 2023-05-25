a088b295f08c68ca5cba0d52357238f9c3b808cb
MattiaBazzani <mattia.bazzani.one@gmail.com>
2023-05-25 23:06:55 +0200

    Update

--- src/routes/02_exercises/03_interactivity/exercise_22/Exercise.svelte
+++ src/routes/02_exercises/03_interactivity/exercise_22/Exercise.svelte
@@ -1,3 +1,12 @@
 <script>
-    
+  let count = 0;
 </script>
+
+<button type="button" class="btn btn-primary btn-sm" on on:click={() => count++}>
+    Cliked {count}
+    {count === 1 ? "time" : "times"}
+</button>
+
+<button type ="button" class="btn btn-danger" on:click={() => (count = 0)}>
+    Reset
+</button>
